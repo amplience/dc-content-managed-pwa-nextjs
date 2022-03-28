4d1afbbff0c356fc72c9248df40a09dfff81fd73
Liliya Mykhailova <lm@codemotion.eu>
2022-03-28 21:21:45 +0300

    chore: test new storybook libraries versions

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -10,7 +10,7 @@ const docGenLoader = {
 };
 
 module.exports = {
-    stories: ['../**/*.stories.(tsx|mdx)'],
+    stories: ['../**/*.stories.@(tsx|mdx)'],
     addons: [
         '@storybook/addon-links/register',
         {
